266ba33db19208040053822558947a79ef7e5e0c
my pc <snehalbhongade7@gmail.com>
2019-05-06 18:51:26 +0530

    updated main method

--- src/com/model/Screenshotwithdatetime.java
+++ src/com/model/Screenshotwithdatetime.java
@@ -28,6 +28,7 @@ public class Screenshotwithdatetime extends s1{
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
+		System.out.println("main method");
 		System.setProperty("webdriver.gecko.driver", "D:\\Selenium Setup\\firefox\\geckodriver.exe");
 		 s1.driver = new FirefoxDriver();
 		driver.get("http://www.facebook.com");
